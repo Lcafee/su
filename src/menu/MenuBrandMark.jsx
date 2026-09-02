5b6379d8a0fa10a85d88af745000ba16bfd21d00
Lcafee <lcafeesf@gmail.com>
2026-09-03 03:11:27 +0330

    Replace menu placeholder with owner brand symbol

--- src/menu/MenuBrandMark.jsx
+++ src/menu/MenuBrandMark.jsx
@@ -1,9 +1,16 @@
 import { memo } from "react";
 
+import { sitePath } from "../sitePath";
+
 export const MenuBrandMark = memo(function MenuBrandMark() {
   return (
-    <span className="menu-brand-mark" aria-hidden="true">
-      L
-    </span>
+    <img
+      className="menu-brand-mark"
+      src={sitePath("assets/brand/l-cafe-symbol.png")}
+      width="6000"
+      height="8044"
+      alt=""
+      aria-hidden="true"
+    />
   );
 });
