87b4f46df9ce478744beecde6b6278a635ce3aa9
Lcafee <lcafeesf@gmail.com>
2026-09-19 20:43:25 +0330

    feat(menu): add mobile background concept previews

--- src/menu/main.jsx
+++ src/menu/main.jsx
@@ -4,9 +4,11 @@ import { createRoot } from "react-dom/client";
 import { MenuApp, MenuMasthead } from "./MenuApp";
 import { MenuRuntime } from "./MenuRuntime";
 import "../styles/menu.css";
+import { MenuBackgrounds } from "./MenuBackgrounds";
 
 createRoot(document.getElementById("menu-root")).render(
   <StrictMode>
+    <MenuBackgrounds />
     <MenuRuntime AppComponent={MenuApp} MastheadComponent={MenuMasthead} />
   </StrictMode>,
 );
